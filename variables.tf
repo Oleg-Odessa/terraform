510e08fe46776cfb2b2346d083c272101af451af
Oleg-Odessa <62992559+Oleg-Odessa@users.noreply.github.com>
2021-09-15 20:43:59 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+    features {}
+    subscription_id = "${var.subscription_id}"
+    tenant_id       = "${var.tenant_id}"
+}
+
+variable "subscription_id" { description = "value"}
+
+variable "tenant_id" { description = "value" }
+
+variable "name" { default = "EIS"}
+
+variable "location" {default = "West Europe"}
+
+variable "prefix" { default = "TF"}
